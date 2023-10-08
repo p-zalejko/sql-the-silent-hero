5e7a7e62ff26b5c6c91836cdf1b0ae09ec7683d2
Pawel Zalejko <p.zalejko@gmail.com>
2023-10-08 22:25:13 +0200

    update

--- demo-app/src/main/resources/sql-examples/7.sql
+++ demo-app/src/main/resources/sql-examples/7.sql
@@ -1,5 +1,39 @@
-?????
+-- prepared statements
 
--- get next/previous record
+-- When the PREPARE statement is executed, the specified statement is parsed, analyzed, and rewritten
+-- When an EXECUTE command is subsequently issued, the prepared statement is planned and executed
 
-https://twitter.com/sqldaily/status/1642844437452652544?t=pIEls20K0cVLYvtO7N0Slg&s=19
+
+PREPARE findByCategory (text) AS
+    SELECT
+        category.category_id,
+        category.name,
+        film.film_id,
+        film.title
+    FROM film_category
+             JOIN category USING (category_id)
+             JOIN film USING (film_id)
+    WHERE name = $1;
+
+-- DEALLOCATE findByCategory;
+
+
+EXECUTE findByCategory('Comedy');
+EXECUTE findByCategory('Sci-Fi');
+
+-- generic plan vs custom plan
+
+EXPLAIN (ANALYSE ) EXECUTE findByCategory('Documentary');
+
+EXPLAIN (ANALYSE )SELECT
+    category.category_id,
+    category.name,
+    film.film_id,
+    film.title
+FROM film_category
+    JOIN category USING (category_id)
+    JOIN film USING (film_id)
+    WHERE name = 'Documentary'
+
+
+
